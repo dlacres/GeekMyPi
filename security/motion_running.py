874bb262a895b8a930e2ffed155f2eba81a38e38
dlacres <dlacres@gmail.com>
2015-06-01 07:07:26 -0500

    check if motion is running

---
+++ security/motion_running.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import re
+
+def process_exists(proc_name):
+    ps = subprocess.Popen("ps ax -o pid= -o args= ", shell=True, stdout=subprocess.PIPE)
+    ps_pid = ps.pid
+    output = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+
+    for line in output.split("\n"):
+        res = re.findall("(\d+) (.*)", line)
+        if res:
+            pid = int(res[0][0])
+            if proc_name in res[0][1] and pid != os.getpid() and pid != ps_pid:
+                return True
+    return False
+
+print process_exists('motion')
+
